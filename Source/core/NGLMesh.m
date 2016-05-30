76ddf1c31ec0bef152c055ed5d1013ef1f15ec2a
Gauthier Monserand <github@simkim.net>
2016-05-30 16:20:24 +0200

    remove __weak keyword

--- Source/core/NGLMesh.m
+++ Source/core/NGLMesh.m
@@ -661,7 +661,7 @@ static void emptyCoreMesh(id <NGLCoreMesh> coreMesh)
 	{
         // AH: Really need a language upgrade to use ARC and weak references
         // to deal with potentially deallocated threads on async main dispatch.
-        __weak NGLMesh *weakSelf = self;
+        /* __weak */ NGLMesh *weakSelf = self;
 		dispatch_async(dispatch_get_main_queue(), ^(void)
 		{
             NGLMesh *safeSelf = weakSelf;
